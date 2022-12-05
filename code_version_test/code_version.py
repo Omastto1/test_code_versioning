6d1fde6d77610176d792e61fa7737002d3f859b0
Tomas Omasta <omastto1@fel.cvut.cz>
2022-12-05 19:26:09 +0100

    code versioning commit on 2022-12-05

--- code_version_test/code_version.py
+++ code_version_test/code_version.py
@@ -4,5 +4,3 @@ fc = flp.flcore.ForloopClient(project_key=..., url=...)
 
 pipe1 = fc.new_pipeline('immobilier')
 
-pipe1.add(flp.nodes.DropColumn(df_entry="", column_name="[]", new_var_name=""))
-pipe1.add(flp.nodes.Replace(df_entry="", columns="[]", match="pattern", replace_substring="False", pattern="", replacement="", new_var_name=""))
